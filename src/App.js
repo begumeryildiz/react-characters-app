f339438521ac35242ac42d2214ba28b6dc3fc826
Begum Eryildiz <begumeryildiz@gmail.com>
2022-07-28 15:13:47 +0200

    added delete functionality

--- src/App.js
+++ src/App.js
@@ -1,44 +1,71 @@
-import { useEffect, useState } from 'react';
 import './App.css';
-import axios from "axios"
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
 
-  const baseURL = "https://ih-crud-api.herokuapp.com"
+  const baseURL = 'https://ih-crud-api.herokuapp.com';
 
-  const [characters, setCharacters] = useState([]);
+  const [characters, setCharacters] = useState(null);
 
-  useEffect(() => {
-    axios
-      .get(baseURL + "/characters")
-      .then(response => {
-        setCharacters(response.data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-
+  useEffect( () => {
+    fetchCharacters();
   }, []);
+
+
+  const fetchCharacters = () => {
+    axios
+      .get(baseURL + '/characters')
+      .then((response) => {
+        const allCharacters = response.data;
+        const fistTen = allCharacters.slice(0, 10)
+        setCharacters(fistTen);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }
+
+
+  const renderCharacters = () => {
+    const result = characters.map((element, index) => {
+      return (
+        <div key={index} className='Characters'>
+          <h2>{element.name}</h2>
+          <p>Occupation: {element.occupation}</p>
+          <p>Weapon: {element.weapon}</p>
+
+          <button onClick={() => { deleteCharacter(element.id) }}>Delete</button>
+        </div>
+      );
+    });
+
+    return result;
+  }
+
+  const deleteCharacter = (id) => {
+    axios
+      .delete(baseURL + '/characters/' + id)
+      .then((response) => {
+        fetchCharacters();
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }
 
   return (
     <div className="App">
       <header>
-        <h1>React Characters App</h1>
+        <h1>React Charates App</h1>
       </header>
 
       <div>
-        {characters.slice(0,11).map((element,index) => {
-          return (
-            <div className='Characters' key={index}>
-              <h3>Name:{element.name}</h3>
-              <p>Occupation:{element.occupation}</p>
-              <p>Weapon: {element.weapon}</p>
-              <button>Delete</button>
-            </div>)
-        })}
+        { characters === null
+          ? <p>loading...</p>
+          : renderCharacters()
+        }
       </div>
-
-      <footer>This the footer</footer>
 
     </div>
   );
