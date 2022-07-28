f219e65d2eba1f320cfc4fa1c707757b1e95943c
Begum Eryildiz <begumeryildiz@gmail.com>
2022-07-28 14:31:04 +0200

    listing first 11 characters

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import axios from "axios"
 
 function App() {
+
+  const baseURL = "https://ih-crud-api.herokuapp.com"
+
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(baseURL + "/characters")
+      .then(response => {
+        setCharacters(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>React Characters App</h1>
       </header>
+
+      <div>
+        {characters.slice(0,11).map((element,index) => {
+          return (
+            <div className='Characters' key={index}>
+              <h3>Name:{element.name}</h3>
+              <p>Occupation:{element.occupation}</p>
+              <p>Weapon: {element.weapon}</p>
+              <button>Delete</button>
+            </div>)
+        })}
+      </div>
+
+      <footer>This the footer</footer>
+
     </div>
   );
 }
